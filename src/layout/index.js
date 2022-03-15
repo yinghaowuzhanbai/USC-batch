6474e636cba4127124264732c16791aeceb34c73
xinyuthedeveloper <wilsonyu0626@gmail.com>
2022-03-15 10:44:25 -0700

    settup page routes

---
+++ src/layout/index.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../container/Home";
+import Navbar from "../component/Navbar";
+import MovieList from "../container/MovieList";
+import LikedList from "../container/LikedList";
+import BlockedList from "../container/BlockedList";
+
+export default function Layout() {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie" element={<MovieList />} />
+          <Route path="liked" element={<LikedList />} />
+          <Route path="blocked" element={<BlockedList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
