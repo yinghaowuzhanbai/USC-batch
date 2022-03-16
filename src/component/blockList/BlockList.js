203cdc420b7fdcf3144493a37fae3d08a16f45ef
xinyuthedeveloper <wilsonyu0626@gmail.com>
2022-03-16 11:17:39 -0700

    renaming files and component

--- src/component/blockList/BlockList.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-export default function BlockList() {
-  const [isBlocked, setIsBlocked] = useState(false);
-  const [blockList, setBlockList] = useState([]);
-
-  return <h1>blocked list</h1>;
-}
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../container/home/Home";
-import Navbar from "../component/navbar/Navbar";
-import MovieList from "../container/movieList/MovieList";
-import LikedList from "../container/likedList/LikedList";
-import BlockedList from "../container/blockedList/BlockedList";
+import Home from "../container/Home/index";
+import Navbar from "../component/Navbar";
+import MovieList from "../container/MovieList";
+import LikedList from "../container/LikedList";
+import BlockedList from "../container/BlockedList";
 
 export default function Layout() {
   return (
