667ea97b3eb559f40025384249d90aa22a8cd954
Xin Yu <74640008+xinyuthedeveloper@users.noreply.github.com>
2022-03-16 13:39:53 -0700

    Revert "File organize"

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../container/Home/index";
+import Home from "../container/Home";
 import Navbar from "../component/Navbar";
 import MovieList from "../container/MovieList";
 import LikedList from "../container/LikedList";
