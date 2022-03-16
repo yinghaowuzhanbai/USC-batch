7ce96b339403134761dc06bfe0b064fcd6bf9c2f
“sinan“ <sinan.liu001@umb.edu>
2022-03-16 12:04:34 -0400

    react-script-update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+
+  return (
+    <nav className="Nav">
+          <div className="Nav__container">
+
+          </div>
+    </nav>
+  );
 }
