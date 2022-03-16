b921af81995767aa14613a726a2c68e4b90f668f
“sinan“ <sinan.liu001@umb.edu>
2022-03-16 19:34:53 -0400

    disable&loading

--- src/container/MovieList/index.js
+++ src/container/MovieList/index.js
@@ -1,28 +1,22 @@
 import React from "react";
-import './Home.css'
+import './style.css'
 import {useState, useEffect} from "react";
 import {createStore} from 'redux';
-import like from '../store/like.js'
+import store from '../../utils/actionCreator.js'
 
-const likestore = createStore(like, []);
 // [ 'Use Redux', 'Read the docs' ]  
 export default function MovieList() {
   const [data, setData] = useState([])
   const [page, setPage] = useState(1)
-
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    const fetchData = async() => {
-      const res = await fetch(
+      fetch(
         `https://api.themoviedb.org/3/movie/popular?api_key=f4fd559b706454d3e7876ad1c9d54257&page=${page}`
-      );
-      const json = await res.json();
-      setData(json.results);
-      // likestore.dispatch({
-      //   type:'ADD_LIKE',
-      //   text: page
-      // })
-    }
-    fetchData();
+      ).then(res => res.json())
+      .then(data => {
+        setData(data.results)
+        setLoading(false)})
+      .catch(error => console.log("error"));
   })
   function displayPage(){
     return `${page} page`;
@@ -36,36 +30,56 @@ export default function MovieList() {
       }
     }
   }
+  function addLikeList(){
+    store.disPatch('')
+  }
   return (
     <div className="movie_layout">
+      <div className="listView">
+        <button disabled={loading? true:false}>Title</button>
+        <button disabled={loading? true:false}>Title</button>
+        <button disabled={loading? true:false}>Rate</button>
+        <button disabled={loading? true:false}>Realse-Data</button>
+      </div>
       <div className="pageSwitch">
-        <button onClick={pageRender} id='0'>left</button>
+        <button onClick={pageRender} id='0' disabled={loading? true:false}>left</button>
         <span>{displayPage()}</span>
-        <button onClick={pageRender} id='1'>right</button>
+        <button onClick={pageRender} id='1' disabled={loading? true:false}>right</button>
       </div>
-      <div className="movie_container">
-        {data.map(element =>
-          (
-           
-             <MovieListContainer element={element}/>
-           
-          ))}
-      </div>
-    </div>
+      <div>{loading? <LoadingSpinner/>:<ResultSpinner data={data}/>}</div>
 
+    </div>
   );
 }
+
+function LoadingSpinner(){
+  return <div>loading</div>
+}
+
+function ResultSpinner({data}){
+  return (<div className="movie_container">
+  {data.map(element =>
+    (
+       <MovieListContainer element={element}/>
+    ))}
+  </div>)
+}
+
 function MovieListContainer({element}){
     const stringPath = `https://image.tmdb.org/t/p/w500${element.poster_path}`
     return (
         <div className="movie_element">
           <div><img className="movie_element_pic" src={stringPath} alt=""/></div>
           <span>{element.original_title}</span>
+          <div>
+            <button onClick={}></button>
+            <button onClick={}></button>
+          </div>
           <span>{element.release_date}</span>
           <span>
             {element.overview}
           </span>
         </div>
-)
+  )
     
 }
