2a6a3707868ac1f0bb35577da6b49662011d2819
“sinan“ <sinan.liu001@umb.edu>
2022-03-17 11:02:27 -0400

    button+redux+store

--- src/container/MovieList/index.js
+++ src/container/MovieList/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './style.css'
 import {useState, useEffect} from "react";
-import {createStore} from 'redux';
+import {createStore, dispatch} from 'redux';
 import store from '../../utils/actionCreator.js'
 
+console.log(store.getState());
 // [ 'Use Redux', 'Read the docs' ]  
 export default function MovieList() {
   const [data, setData] = useState([])
@@ -29,12 +30,29 @@ export default function MovieList() {
         setPage(page-1);
       }
     }
+    console.log(store.getState())
   }
-  function addLikeList(){
-    console.log("add")
+  function addLikeList(event){
+    console.log(event.target)
+    const add_item = data.find(element => element.id == event.target.id)
+    console.log(add_item)
+    store.dispatch(
+      {
+        type:'IS_LIKE',
+        text: add_item}
+    )
+    console.log(store.getState())
   }
-  function blockList(){
-    console.log("block")
+  function blockList(event){
+    console.log(event.target)
+    const add_item = data.find(element => element.id == event.target.id)
+    console.log(add_item)
+    store.dispatch(
+      {
+        type:'IS_BLOCK',
+        text: add_item}
+    )
+    console.log(store.getState())
   }
   return (
     <div className="movie_layout">
@@ -70,19 +88,20 @@ function ResultSpinner({data, addLikeList, blockList}){
 
 function MovieListContainer({element, addLikeList, blockList}){
     const stringPath = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+    // const check = store.find(item => item.id == element).isBlocked;
     return (
+      <div>
         <div className="movie_element">
           <div><img className="movie_element_pic" src={stringPath} alt=""/></div>
           <span>{element.original_title}</span>
           <div>
-            <button onClick={addLikeList}></button>
-            <button onClick={blockList}></button>
+            <button onClick={addLikeList} id={element.id}>LIKE</button>
+            <button onClick={blockList} id={element.id}>BLOCK</button>
           </div>
           <span>{element.release_date}</span>
           <span>
             {element.overview}
           </span>
-        </div>
-  )
-    
+        </div></div>
+  ) 
 }
--- src/utils/actionCreator.js
+++ src/utils/actionCreator.js
@@ -1,17 +1,43 @@
 import { ADD_TO_LIKEDLIST, REMOVE_FROM_LIKEDLIST } from "./actions";
 import { createStore } from "redux";
 
-const initialState = {
-  movies: [],
-  isLiked: false,
-  isBlocked: false,
+// const initialState = {
+//   movie: '',
+//   id: ''
+//   isLiked: false,
+//   isBlocked: false,
+// };
+
+
+const reducer = (state = [], action) => {
+  switch(action.type){
+    case 'IS_LIKE':
+      const check1 = state.find(element => element.id === action.text.id);
+      if (!check1){
+        return state.concat(
+          {
+            movie: action.text,
+            id: action.text.id,
+            isLiked: true,
+            isBlocked: false});
+      }else {
+        return state;
+      }
+    case 'IS_BLOCK':
+      const check2 = state.find(element => element.id === action.text.id);
+      if (!check2){
+        return state.concat(
+          {
+            movie: action.text,
+            id: action.text.id,
+            isLiked: false,
+            isBlocked: true});
+      }else {return state}
+    default:
+      return state;
+  }
 };
 
-const reducer = (state = initialState, action = {}) => {
-  console.log({ state, action });
-  return state;
-};
-
-const store = createStore(reducer, initialState);
+const store = createStore(reducer);
 
 export default store;
