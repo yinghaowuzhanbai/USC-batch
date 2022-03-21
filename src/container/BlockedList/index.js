bb7e82f76444361b6160f30cca7fc38b78fec7a7
xinyuthedeveloper <wilsonyu0626@gmail.com>
2022-03-21 10:05:39 -0700

    restore missing commits

--- src/container/BlockedList/index.js
+++ src/container/BlockedList/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BlockList from "../../component/BlockList";
 import store from "../../utils/actionCreator";
-import "./index.css";
+// import "./index.css";
 
 export default function BlockedList() {
   const movies = store.getState();
