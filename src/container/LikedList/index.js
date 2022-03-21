1ec70ea1cacb9bdffdff5f0361af25080917e168
yinghaowuzhanbai <55349518+yinghaowuzhanbai@users.noreply.github.com>
2022-03-21 10:07:33 -0400

    change on likelist

--- src/container/LikedList/index.js
+++ src/container/LikedList/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import LikeList from "../../component/LikeList";
+import store from "../../utils/actionCreator";
 
 export default function LikedList() {
-  return <h1>Liked list</h1>;
+  const movies = store.getState();
+  return (
+    <div className="blocked-grid">
+      {movies.map(({ movie, isLiked }) => {
+        return isLiked && <LikeList movie={movie} />;
+      })}
+    </div>
+  );
 }
