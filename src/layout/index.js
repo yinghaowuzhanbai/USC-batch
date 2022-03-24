07fccdd47083cbf8d29e7bd40d16d3d06e8b3810
“sinan“ <sinan.liu001@umb.edu>
2022-03-24 13:44:02 -0400

    package_problem

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import React from 'react';
 import Home from "../container/Home/index";
 import Navbar from "../component/Navbar";
 import MovieList from "../container/MovieList";
