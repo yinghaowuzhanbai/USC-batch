884d0dba656cfb2893347cccbbc3b4cd7d8dafa3
yinghaowuzhanbai <55349518+yinghaowuzhanbai@users.noreply.github.com>
2022-03-24 12:25:31 -0400

    Update index.js

--- src/component/LikeList/index.js
+++ src/component/LikeList/index.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import store from "../../utils/actionCreator";
+import Detail from "../BlockList/Detail";
+import "./index.css";
 
-export default function LikeList({ movie }) {
-  console.log(movie);
+export default function LikeList({ movie, isLiked }) {
+  const data = store.getState();
+  const [isShow, setIsShow] = useState(isLiked);
+  const [showDetail, setShowDetail] = useState(false);
+
+  const buttonsHandler = (event) => {
+    const movieFound = data.find(
+      (element) => element.id === Number(event.target.parentNode.id)
+    );
+    console.log(movieFound);
+    if (event.target.name === "unlike" || event.target.name === "block") {
+      if (event.target.name === "block") {
+        store.dispatch({
+          type: "IS_BLOCK",
+          text: movieFound,
+        });
+      }
+      store.dispatch({
+        type: "UNLIKE",
+        text: movieFound,
+      });
+      setIsShow(!isShow);
+    }
+
+    if (event.target.name === "detail") {
+      setShowDetail(true);
+    }
+  };
+
   return (
-    <div className="liked-movie">
-      <img
-        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-        alt={movie.original_title}
-      />
-      <div className="funtion-buttons">
-        <button>Delete</button>
-        <button>Block</button>
-        <button>Unlike</button>
+    isShow && (
+      <div
+        className="liked-movie"
+        id={movie.id}
+        onClick={(e) => buttonsHandler(e)}
+      >
+        {showDetail && (
+          <Detail
+            movie={movie}
+            showDetail={showDetail}
+            setShowDetail={setShowDetail}
+          />
+        )}
+        <div className="image-wrapper" id={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.original_title}
+          />
+          <div className="buttons" id={movie.id}>
+            <button name="unlike">Delete</button>
+            <button name="block">Block</button>
+            <button name="detail">Detail</button>
+          </div>
+        </div>
       </div>
-      <p>{movie.original_title}</p>
-    </div>
+    )
   );
 }
